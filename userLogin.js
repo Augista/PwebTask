9342834a4720fbfcc2a3fb9d0859e3a3155eeb6d
Augista <augistapradana11@gmail.com>
2024-09-18 12:57:09 +0700

    paage

---
+++ userLogin.js
@@ -0,0 +1,17 @@
+document.getElementById('LihatPassword').addEventListener('change', function() {
+    const passwordField = document.getElementById('password');
+    if (this.checked) {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
+});
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    window.location.href = 'index.html';
+});
